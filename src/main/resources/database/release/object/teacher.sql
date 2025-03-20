900bb86f21addb97ec4a41a050f74e5b7056ec5c
chirikbombom228 <olesya.vorobeva.2006@gmail.com>
2025-03-20 19:46:20 +0300

    сделаль таблицы для бд, передайте яри

--- src/main/resources/database/release/object/teacher.sql
+++ src/main/resources/database/release/object/teacher.sql
@@ -3,11 +3,11 @@
 --changeset olesyaTop: teacher-1
 
 CREATE TABLE IF NOT EXISTS sport_schema.teacher_table (
-    id  INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    fsp VARCHAR(100) NOT NULL UNIQUE,
-    is_moderator BOOLEAN NOT NULL,
-    schedule JSONB NOT NULL,
-    login VARCHAR NOT NULL UNIQUE,
-    password_hash VARCHAR NOT NULL,
+    id               INTEGER       NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    fsp              VARCHAR(100)  NOT NULL UNIQUE,
+    is_moderator     BOOLEAN       NOT NULL,
+    schedule         JSONB         NOT NULL,
+    login            VARCHAR       NOT NULL UNIQUE,
+    password_hash    VARCHAR       NOT NULL,
 
 )
