4eeffb188802cf5af837f9595a33f399bfd09a33
Nikita <net058230@gmail.com>
2026-03-13 01:01:17 +0300

    Создание DisciplineEntity. НЕПОЛНАЯ переделка TeacherEntity. Комментарии в TeacherServiceImpl для исправления.

--- src/main/java/com/sport/project/dao/entity/TeacherEntity.java
+++ src/main/java/com/sport/project/dao/entity/TeacherEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
@@ -16,18 +17,38 @@ import java.util.*;
 @NoArgsConstructor
 public class TeacherEntity extends BaseEntity<Integer> implements Serializable {
 
+    @Setter
+    private String firstName;
+    @Setter
+    private String lastName;
+    @Setter
+    private String patronymic;
+    @Setter
+    private Date birthday;
+
+    @Setter
     private boolean isModerator;
+
     private Map<LocalDate, String> schedule = new HashMap<>();
     private List<StudentEntity> students = new ArrayList<>();
 
 
-    public TeacherEntity(String fsp,
+    public TeacherEntity(String firstName,
+                         String lastName,
+                         String patronymic,
+                         Date birthday,
                          boolean isModerator,
                          Map<LocalDate, String> schedule,
                          String login,
-                         String passwordHash)
-    {
-        super(fsp, login, passwordHash);
+                         String passwordHash) {
+        //FIXME: Из-за наследования необходимо цельное ФИО, собираемое по частям. Исправить когда будет разделен.
+        super(createFullName(firstName, lastName, patronymic), login, passwordHash);
+
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setPatronymic(patronymic);
+        this.setBirthday(birthday);
+
         this.setModerator(isModerator);
         this.setSchedule(schedule);
     }
@@ -41,6 +62,7 @@ public class TeacherEntity extends BaseEntity<Integer> implements Serializable {
         return this.id;
     }
 
+    //TODO: вариант с цельным ФИО для совместимости. Исправить на раздельный.
     @Column(name = "fsp", unique = true, nullable = false)
     @Override
     public String getFsp() {
@@ -59,9 +81,10 @@ public class TeacherEntity extends BaseEntity<Integer> implements Serializable {
         return this.passwordHash;
     }
 
-    @Column(name = "is_moderator", nullable = false)
-    public Boolean isModerator() {
-        return this.isModerator;
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "schedule", columnDefinition = "JSONB", nullable = false)
+    public Map<LocalDate, String> getSchedule() {
+        return this.schedule;
     }
 
     @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
@@ -69,18 +92,10 @@ public class TeacherEntity extends BaseEntity<Integer> implements Serializable {
         return this.students;
     }
 
-
-    public void setModerator(boolean isModerator) {
-        this.isModerator = isModerator;
+    @Column(name = "is_moderator", nullable = false)
+    public Boolean isModerator() {
+        return this.isModerator;
     }
-
-
-    @JdbcTypeCode(SqlTypes.JSON)
-    @Column(name = "schedule", columnDefinition = "JSONB", nullable = false)
-    public Map<LocalDate, String> getSchedule() {
-        return this.schedule;
-    }
-
 
     public void setSchedule(@NonNull Map<LocalDate, String> schedule) {
         this.schedule = schedule;
@@ -95,30 +110,51 @@ public class TeacherEntity extends BaseEntity<Integer> implements Serializable {
         return new TeacherEntityBuilder();
     }
 
-    private boolean isValidFsp(String fsp) {
-        char[] chars = fsp.toCharArray();
-        int k = 0;
-        for (char c : chars) {
-            if (c == ' ') {
-                k++;
-            }
-        }
 
-        return k == 2;
+    private static String createFullName(String firstName, String lastName, String patronymic) {
+        return firstName + " " + lastName + " " + patronymic;
     }
 
+// FIXME: нигде не используемый метод. Будет не нужен когда будет раздельный ФИО. Убрать.
+//
+//    private boolean isValidFsp(String fsp) {
+//        char[] chars = fsp.toCharArray();
+//        int k = 0;
+//        for (char c : chars) {
+//            if (c == ' ') {
+//                k++;
+//            }
+//        }
+//
+//        return k == 2;
+//    }
 
+    //  FIXME: Нет возможности нормально переписать builder из-за старого сервиса и цельного ФИО в интерфейсе Entity.
     public static final class TeacherEntityBuilder {
 
-        private String fsp;
+        private String firstName;
+        private String lastName;
+        private String patronymic;
+        private Date birthday;
+
         private boolean isModerator;
         private Map<LocalDate, String> schedule;
         private String login;
         private String passwordHash;
 
 
-        public TeacherEntityBuilder fsp(String fsp) {
-            this.fsp = fsp;
+        public TeacherEntityBuilder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public TeacherEntityBuilder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public TeacherEntityBuilder patronymic(String patronymic) {
+            this.patronymic = patronymic;
             return this;
         }
 
@@ -132,6 +168,11 @@ public class TeacherEntity extends BaseEntity<Integer> implements Serializable {
             return this;
         }
 
+        public TeacherEntityBuilder birthday(Date birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
         public TeacherEntityBuilder login(String login) {
             this.login = login;
             return this;
@@ -142,9 +183,30 @@ public class TeacherEntity extends BaseEntity<Integer> implements Serializable {
             return this;
         }
 
+        // Только для временной совместимости со старым сервисом.
+        // FIXME: Сейчас делит ФИО на раздельные. Нужно убрать когда будет разделенное на БД и сервисах.
+        @Deprecated
+        public TeacherEntityBuilder fsp(String fsp) {
+            if (fsp != null) {
+                String[] parts = fsp.split(" ");
+                if (parts.length == 3) {
+                    this.firstName = parts[0];
+                    this.lastName = parts[1];
+                    this.patronymic = parts[2];
+                } else {
+                    // На случай если плохой формат.
+                    this.firstName = fsp;
+                }
+            }
+            return this;
+        }
+
         public TeacherEntity build() {
             return new TeacherEntity(
-                    this.fsp,
+                    this.firstName,
+                    this.lastName,
+                    this.patronymic,
+                    this.birthday,
                     this.isModerator,
                     this.schedule,
                     this.login,
