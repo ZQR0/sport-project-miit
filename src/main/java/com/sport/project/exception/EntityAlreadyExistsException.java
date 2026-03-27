c83c42b0ca5d34f8bbaf86e5e1f43c5756649b80
ZQR0 <yarikrech@gmail.com>
2026-03-27 11:15:54 +0300

    Exceptions: exception class --> runtime exception

--- src/main/java/com/sport/project/exception/EntityAlreadyExistsException.java
+++ src/main/java/com/sport/project/exception/EntityAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.sport.project.exception;
 
-public class EntityAlreadyExistsException extends Exception {
+public class EntityAlreadyExistsException extends RuntimeException {
     public EntityAlreadyExistsException(String message) {
         super(message);
     }
